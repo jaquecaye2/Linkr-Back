290e03c125907f5b0b24d3f177d0740b800f19f6
MariaDuda <mariabevenuto062@gmail.com>
2022-08-09 19:10:43 -0300

    feat users from thales

--- src/ routers/index.js
+++ src/ routers/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import hastagRoute from "./hastagRoute.js";
+import userRouter from "./userRouter.js";
 
 const router = Router();
 
 router.use(hastagRoute)
+router.use(userRouter)
 
 export default router
