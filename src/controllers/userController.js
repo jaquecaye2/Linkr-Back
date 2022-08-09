9a1f8bd74eee969853a4c5c5eb0ba2732cb6098e
Thales Alves <thalesalves758@gmail.com>
2022-08-09 19:23:18 -0300

    fix: send 404 status code if not find users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,16 @@ import userRepository from "../repositories/userRepository.js";
 import httpStatus from "../utils/httpStatus.js";
 
 async function getUsers(req, res) {
+  const ZERO = 0;
   const { name = "" } = req.query;
 
   try {
     const users = await userRepository.getUsersWithName(name);
+
+    if (users.length === ZERO) {
+      res.sendStatus(httpStatus.NOT_FOUND);
+      return;
+    }
 
     res.send(users);
   } catch (error) {
