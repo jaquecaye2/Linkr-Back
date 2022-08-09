1fda81de797e30ca9c3458c6bed24c53e8aa97d5
= <=>
2022-08-09 17:38:23 -0300

    meddleware tokenValidator

--- src/ routers/authRoutes.js
+++ src/ routers/authRoutes.js
@@ -5,10 +5,15 @@ import signInSchema from "../schemas/signInSchema.js";
 import { logIn } from "../controllers/auth/logIn.js";
 import logInSchema from "../schemas/logInSchema.js";
 
+import { tests } from "../controllers/auth/testsMeddlewares.js";
+import { tokenValidator } from "../middlewares/tokenValidator.js";
+
 
 const authRouter = Router()
 
 authRouter.post('/singin',validateSchema(signInSchema), signIn)
 authRouter.post('/login',validateSchema(logInSchema), logIn)
+
+authRouter.post('/tests', tokenValidator, tests)
 
 export default authRouter
