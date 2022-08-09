b4cd25700f9bc563fce720b825084b4443041140
MariaDuda <mariabevenuto062@gmail.com>
2022-08-09 11:00:45 -0300

    feat search hastag && redirect hastag

--- src/controllers/hastagController.js
+++ src/controllers/hastagController.js
@@ -3,11 +3,16 @@ import hastagRepository from "../repositories /hastagRepository.js"
 export async function hastag(req,res){
     const { hastag } = req.params;
     try{
-        const findHastag=  await hastagRepository.findHastag(hastag)
-        if(!findHastag){
+        const  findHastag =  await hastagRepository.findHastag(hastag)
+        if(findHastag.rowCount == 0){
            return res.status(404).send("hastag Not Found")
         }
-        res.status(200).send("oi")
+
+        const redirectHastag = await hastagRepository.redirectHastag(hastag)
+        console.log(redirectHastag)
+        
+
+        res.status(200).send("ok")
     }catch(e){
         console.log(e)
         res.status(500).send("erro na hastag")
--- src/repositories /hastagRepository.js
+++ src/repositories /hastagRepository.js
@@ -5,8 +5,20 @@ async function findHastag(hastag) {
 }
 
 
+async function redirectHastag(hastag) {
+  return db.query(`
+  SELECT u.name, u.id AS user_id, u.picture, p.link, p.id as "postId", p.description, p.created_at 
+  FROM  posts_hastgs pt
+  JOIN posts p ON p.id = pt.post_id
+  JOIN users u ON p.user_id = u.id
+  JOIN hastags h ON h.id = pt.hastag_id
+  WHERE h.name = $1
+  `, [hastag]);
+}
+
 const hastagRepository = {
     findHastag,
+    redirectHastag
 
 }
 
