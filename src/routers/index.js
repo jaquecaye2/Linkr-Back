d83a4108f8fc03bf36762d6b85e761904c9ee48a
jaquecaye <jaquelinecaye@gmail.com>
2022-08-11 18:11:08 -0300

    Inclusão da barra de pesquisa

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import hastagRoute from "./hastagRoute.js";
 import postRouter from "./postRouter.js"
+import userRouter from "./userRouter.js";
 
 const router = Router();
 
+router.use(userRouter);
 router.use(hastagRoute)
 router.use(postRouter)
 
