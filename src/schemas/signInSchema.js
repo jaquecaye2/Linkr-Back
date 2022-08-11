c1727bd3aafe150cab9cf427460fc2a12e75a3da
MariaDuda <mariabevenuto062@gmail.com>
2022-08-11 13:05:53 -0300

    feat uri in validation

--- src/schemas/signInSchema.js
+++ src/schemas/signInSchema.js
@@ -5,7 +5,7 @@ const signInSchema = Joi.object({
     name: Joi.string().trim().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    pictureUrl: Joi.string().required()
+    pictureUrl: Joi.string().uri().required()
 })
 
 export default(signInSchema)
