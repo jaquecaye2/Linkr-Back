baca658d85b4967440153dc81d2d493ef09aa3fc
Thales Alves <thalesalves758@gmail.com>
2022-08-11 22:05:19 -0300

    teste

--- src/schemas/logInSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const logInSchema = Joi.object({
-
-    email: Joi.string().trim().email().required(),
-    password: Joi.string().trim().required()
-})
-
-export default logInSchema
