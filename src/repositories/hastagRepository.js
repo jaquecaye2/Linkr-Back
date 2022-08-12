45e4c49dce13c10d53a37259044a262a080d749a
Thales Alves <thalesalves758@gmail.com>
2022-08-12 20:39:58 -0300

    feat: add fields into select query

--- src/repositories/hastagRepository.js
+++ src/repositories/hastagRepository.js
@@ -4,40 +4,47 @@ async function findHastag(hastag) {
   return db.query(`SELECT * FROM hastags WHERE name ILIKE $1`, [`%${hastag}%`]);
 }
 
-
 async function redirectHastag(hastag) {
-  return db.query(`
-  SELECT u.name, u.id AS user_id, u.picture, p.link, p.id as post_id, p.description, p.created_at , h.name AS hastag
-  FROM  posts_hastgs pt
-  JOIN posts p ON p.id = pt.post_id
-  JOIN users u ON p.user_id = u.id
-  JOIN hastags h ON h.id = pt.hastag_id
-  WHERE h.name ILIKE $1
-  `, [`%${hastag}%`]);
+  return db.query(
+    `
+    SELECT u.name, u.id AS user_id, u.picture, p.link, p.link_title, p.link_description, p.link_image, p.id as post_id, p.description, p.created_at , h.name AS hastag
+    FROM  posts_hastgs pt
+    JOIN posts p ON p.id = pt.post_id
+    JOIN users u ON p.user_id = u.id
+    JOIN hastags h ON h.id = pt.hastag_id
+    WHERE h.name ILIKE $1
+  `,
+    [`%${hastag}%`]
+  );
 }
 
-
-async function updateView(sum,hastag) {
-  return db.query(`
+async function updateView(sum, hastag) {
+  return db.query(
+    `
   UPDATE hastags 
   SET views = $1
   WHERE name ILIKE $2
-  `, [sum,`%${hastag}%`]);
+  `,
+    [sum, `%${hastag}%`]
+  );
 }
 
-async function rankingHastags(sum,hastag) {
-  return db.query(`
+async function rankingHastags(sum, hastag) {
+  return db.query(
+    `
   SELECT h.name FROM hastags h 
   GROUP BY h.id
   ORDER BY views DESC
-  `, []);
+  `,
+    []
+  );
 }
 
 const hastagRepository = {
-    findHastag,
-    redirectHastag,
-    updateView,
-    rankingHastags
-}
+  findHastag,
+  redirectHastag,
+  updateView,
+  rankingHastags,
+};
 
 export default hastagRepository;
