d8363f55a72154b3144c71be3d18a97683e533a5
MariaDuda <mariabevenuto062@gmail.com>
2022-08-16 10:49:22 -0300

    fix sql tables id && creat comment controller

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,6 +3,7 @@ import hastagRoute from "./hastagRoute.js";
 import postRouter from "./postRouter.js"
 import userRouter from "./userRouter.js";
 import authRouter from "./authRoutes.js";
+import commentsRoute from "./commentsRoute.js";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use(authRouter)
 router.use(userRouter);
 router.use(hastagRoute)
 router.use(postRouter)
+router.use(commentsRoute)
 
 export default router
