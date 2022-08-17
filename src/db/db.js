8c8922bf2af576af5e1ba866ff883d69141907f7
= <=>
2022-08-16 23:29:45 -0300

    shared post

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,30 @@
-import pg from 'pg';
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const user = 'postgres';
+const password = process.env.DB_PASSWORD;
+const host = 'localhost';
+const port = 5432;
+const database = 'linkr';
 
 const { Pool } = pg;
 
-const databaseConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-}
+const configDatabase = {
+  user,
+  password,
+  host,
+  port,
+  database
+};
 
-const db = new Pool(databaseConfig);
+ if(process.env.MODE === "PROD") {
+  configDatabase.ssl = {
+    rejectUnauthorized: false
+  }
+} 
 
+const db = new Pool(configDatabase);
 export default db;
 
