e59f6a6fcdd767275a35382025da235816c7f265
Thales Alves <thalesalves758@gmail.com>
2022-08-19 13:56:52 -0300

    fix: remove user id

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -282,13 +282,10 @@ export async function getPosts(req, res) {
         ? (parseInt(page) - 1) * (limit || DEFAULT_LIMIT)
         : 0;
 
-    const posts = await postRepository.getPostsByUsersIds(
-      [...followedsIds, idUser],
-      {
-        offset,
-        limit,
-      }
-    );
+    const posts = await postRepository.getPostsByUsersIds(followedsIds, {
+      offset,
+      limit,
+    });
 
     if (posts.length === NO_RESULTS) {
       res.status(httpStatus.NOT_FOUND).send("No posts found from your friends");
